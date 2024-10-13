cbb0384e7124c740bc2d1f55a428d3bacc693a97
Murat Cihan Sorkun <mcsorkun@gmail.com>
2024-10-13 16:51:07 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -118,20 +118,21 @@ generated_descriptors = generate(SMILES)
 #Import pretrained models
 mlp_model_import = pickle.load(open('aqsolpred_mlp_model.pkl', 'rb'))
 xgboost_model_import = pickle.load(open('aqsolpred_xgb_model.pkl', 'rb'))
-rf_model_import = pickle.load(open('aqsolpred_rf_model_lite.pkl', 'rb'))
+# rf_model_import = pickle.load(open('aqsolpred_rf_model_lite.pkl', 'rb'))
 
 
 #predict test data (MLP,XGB,RF)
 pred_mlp = mlp_model_import.predict(generated_descriptors)   
 pred_xgb = xgboost_model_import.predict(generated_descriptors)
-pred_rf = rf_model_import.predict(generated_descriptors)   
+# pred_rf = rf_model_import.predict(generated_descriptors)   
 #calculate consensus
-pred_consensus=(pred_mlp+pred_xgb+pred_rf)/3
+# pred_consensus=(pred_mlp+pred_xgb+pred_rf)/3
+pred_consensus=(pred_mlp+pred_xgb)/2
 # predefined_models.get_errors(test_logS_list,pred_enseble)
 
 # results=np.column_stack([pred_mlp,pred_xgb,pred_rf,pred_consensus])
 df_results = pd.DataFrame(SMILES, columns=['SMILES'])
-df_results["LogS (AqSolPred v1.0s)"]=pred_consensus
+df_results["LogS (AqSolPred v1.1s)"]=pred_consensus
 df_results=df_results.round(3)
 
 # df_results.to_csv("results/predicted-"+test_data_name+".csv",index=False)
@@ -153,19 +154,19 @@ generated_descriptors # Skips the dummy first item
 st.write("""
 # About AqSolPred
 
-AqSolPred is an highly accurate solubility prediction model that consists consensus of 3 ML algorithms (Neural Nets, Random Forest, and XGBoost). AqSolPred is developed using a quality-oriented data selection method described in [1] and trained on AqSolDB [2] largest publicly available aqueous solubility dataset.
+AqSolPred is a highly accurate solubility prediction model that consists of a consensus of 3 ML algorithms (Neural Nets, Random Forest, and XGBoost). It is developed using a quality-oriented data selection method described in [1] and trained on AqSolDB [2], the largest publicly available aqueous solubility dataset.
 
-AqSolPred showed a top-performance (0.348 LogS Mean Absolute Error) on Huuskonen benchmark dataset [3].
+AqSolPred showed a top performance (0.348 LogS Mean Absolute Error) on the Huuskonen benchmark dataset [3].
 
-**version:** 1.0s (lite version of v1.0 described in the paper with reduced RFs(n_estimators=200,max_depth=10) but the same performance)
+**version:** 1.1s (lite version of v1.0 described in the paper without random forest model)
 
-If you are using the predictions from AqSolPred on your work, please cite these papers: [1, 2]
+If you are using the predictions from AqSolPred in your work, please cite these papers: [1, 2]
 
 [1] Sorkun, M. C., Koelman, J.M.V.A. & Er, S. (2021). [Pushing the limits of solubility prediction via quality-oriented data selection](https://www.cell.com/iscience/fulltext/S2589-0042(20)31158-5), iScience, 24(1), 101961.
 
 [2] Sorkun, M. C., Khetan, A., & Er, S. (2019).  [AqSolDB, a curated reference set of aqueous solubility and 2D descriptors for a diverse set of compounds](https://www.nature.com/articles/s41597-019-0151-1). Scientific data, 6(1), 1-8.
 
-[3] Huuskonen, J. Estimation of aqueous solubility for a diverse set of organic compounds based on molecular topology. Journal of Chemical Informationand Computer Sciences 40, 773–777 (2000).
+[3] Huuskonen, J. Estimation of aqueous solubility for a diverse set of organic compounds based on molecular topology. Journal of Chemical Information and Computer Sciences 40, 773–777 (2000).
 
 Special thanks: This web app is developed based on the tutorials and the template of [DataProfessor's repository](https://github.com/dataprofessor/code/tree/master/streamlit/part7). 
 
